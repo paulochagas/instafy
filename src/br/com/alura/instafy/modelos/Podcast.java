8c4083a9110a4c3bcd39c1c9f640b7e844268e3c
paulochagas <pachafi@gmail.com>
2024-09-26 08:20:36 +0200

    Instafy

---
+++ src/br/com/alura/instafy/modelos/Podcast.java
@@ -0,0 +1,30 @@
+package br.com.alura.instafy.modelos;
+
+public class Podcast extends Audio {
+    private String apresentador;
+    private String descricao;
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if(this.getTotalCurtidas() > 5000){
+            return 1;
+        }
+        return super.getClassificacao();
+    }
+}
--- src/br/com/alura/instafy/principal/Principal.java
+++ src/br/com/alura/instafy/principal/Principal.java
@@ -1,5 +1,45 @@
+package br.com.alura.instafy.principal;
+
+import br.com.alura.instafy.modelos.Musica;
+import br.com.alura.instafy.modelos.Podcast;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+
+        for (int i = 0; i < 1000 ; i++) {
+
+            minhaMusica.reproduzir();
+
+        }
+
+        for (int i = 0; i < 50; i++) {
+
+            minhaMusica.curtir();
+
+        }
+        //
+        //
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+
+            meuPodcast.reproduzir();
+
+        }
+
+        for (int i = 0; i < 1000; i++) {
+
+            meuPodcast.curtir();
+
+        }
+
+
+
     }
 }
